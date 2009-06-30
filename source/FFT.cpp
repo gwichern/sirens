d8c289b6ae30baa6cd2b3a8f0fb7d0560afb2df3
Brandon Mechtley <bmechtley+github@gmail.com>
2009-06-29 23:11:32 -0700

    external symbols

--- source/FFT.cpp
+++ source/FFT.cpp
@@ -1,5 +1,12 @@
 #include "FFT.h"
 
+extern void fftw_plan_with_nthreads(int nthreads);
+extern fftw_plan fftw_plan fftw_plan_dft_r2c_1d(int n, double *in, fftw_complex *out, unsigned flags);
+extern void fftw_execute(const fftw_plan plan);
+extern void fftw_destroy_plan(fftw_plan plan);
+extern void *fftw_malloc(size_t n);
+extern void fftw_free(void *p);
+
 namespace Sirens {
 	FFT::FFT(int fft_size, int fft_threads, double* input) {
 		fftSize = fft_size;
