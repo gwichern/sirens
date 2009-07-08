c1c81ea52e0748842916d11fc8ab5bcb184d7029
Brandon Mechtley <bmechtley+github@gmail.com>
2009-07-08 16:44:39 -0700

    moved externs

--- source/FFT.cpp
+++ source/FFT.cpp
@@ -1,12 +1,5 @@
 #include "FFT.h"
 
-extern "C" void fftw_plan_with_nthreads(int nthreads);
-extern "C" fftw_plan fftw_plan_dft_r2c_1d(int n, double *in, fftw_complex *out, unsigned flags);
-extern "C" void fftw_execute(const fftw_plan plan);
-extern "C" void fftw_destroy_plan(fftw_plan plan);
-extern "C" void *fftw_malloc(size_t n);
-extern "C" void fftw_free(void *p);
-
 namespace Sirens {
 	FFT::FFT(int fft_size, int fft_threads, double* input) {
 		fftSize = fft_size;
