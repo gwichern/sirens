f1a476603d74f673b67f6e0718684beeae7536a7
Brandon Mechtley <bmechtley+github@gmail.com>
2009-07-08 23:37:31 -0700

    no need for silly externs

--- source/FFT.h
+++ source/FFT.h
@@ -3,13 +3,6 @@
 
 #include <fftw3.h>
 
-extern void fftw_plan_with_nthreads(int nthreads);
-extern fftw_plan fftw_plan_dft_r2c_1d(int n, double *in, fftw_complex *out, unsigned flags);
-extern void fftw_execute(const fftw_plan plan);
-extern void fftw_destroy_plan(fftw_plan plan);
-extern void *fftw_malloc(size_t n);
-extern void fftw_free(void *p);
-
 namespace Sirens {
 	class FFT {
 	private:
