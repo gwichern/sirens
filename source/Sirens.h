f7487c76e356323394a047c2676e63e9f4babc41
Brandon Mechtley <bmechtley+github@gmail.com>
2009-08-16 20:20:03 -0700

    catch-all header that includes all distribution headers

---
+++ source/Sirens.h
@@ -0,0 +1,17 @@
+#ifndef __SIRENS_H__
+#define __SIRENS_H__
+
+#include "CircularArray.h"
+#include "Feature.h"
+#include "FeatureSet.h"
+#include "SegmentationParameters.h"
+#include "Segmenter.h"
+#include "Sound.h"
+#include "features/Harmonicity.h"
+#include "features/Loudness.h"
+#include "features/SpectralCentroid.h"
+#include "features/SpectralSparsity.h"
+#include "features/TemporalSparsity.h"
+#include "features/TransientIndex.h"
+
+#endif
