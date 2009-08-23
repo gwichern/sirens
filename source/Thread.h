aaece72e89b2e48d4b4deeacd35eb93dc2f6014f
Brandon Mechtley <bmechtley+github@gmail.com>
2009-08-23 14:17:51 -0700

    using custom thread class now

--- source/Thread.h
+++ source/Thread.h
@@ -3,15 +3,17 @@
 
 #include <pthread.h>
 
+// Simple thread wrapper to avoid pthread calls.
 class Thread {
 protected:
 	pthread_t thread;
 	
-public:
-	virtual void run() = 0;
+public:	
+	virtual void run() = 0;				// Implement this--is called upon start(). Usually a nonstop loop of some sort.
 	void start();
 };
 
+// Static method for beginning threads with pthread.
 void *start_thread(void* data);
 
 #endif
