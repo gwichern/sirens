9f8b671452971333641bb172e3fc371f4af8938e
Brandon Mechtley <bmechtley+github@gmail.com>
2009-08-23 14:16:27 -0700

    concurrency issues. using pthread now

--- source/Feature.cpp
+++ source/Feature.cpp
@@ -3,42 +3,82 @@
 #include "support/math_support.h"
 
 namespace Sirens {
-	Feature::Feature(int history_size) {
-		history = new CircularArray(history_size, false);
+	Feature::Feature(int history_size) {		
+		history = new CircularArray(history_size);
 		segmentationParameters = new SegmentationParameters();
 		max = 1.0;
 		min = 0.0;
+		
+		recalculate = false;
+		complete = false;
+		running = true;
+		
+		pthread_mutex_init(&mutex, NULL);
+		pthread_cond_init(&recalculateCondition, NULL);
+		pthread_cond_init(&completeCondition, NULL);
 	}
 	
 	Feature::~Feature() {
+		stop();
+		
+		pthread_mutex_destroy(&mutex);
+		pthread_cond_destroy(&recalculateCondition);
+		pthread_cond_destroy(&completeCondition);
+		
 		delete history;
 		delete segmentationParameters;
 	}
 	
-	void Feature::calculate(CircularArray* input_in) {
-		input = input_in;
+	void Feature::run() {		
+		// Continue until no longer running.
+		while (running) {			
+			pthread_mutex_lock(&mutex);		
+				
+			if (!recalculate)
+				pthread_cond_wait(&recalculateCondition, &mutex);
+						
+			recalculate = false;
+			
+			// If the thread stopped running during the wait, don't bother calculating anything. Just exit the loop.
+			if (running) {
+				prepareCalculation();
+				history->addValue(value);
+				
+				complete = true;
+				pthread_cond_signal(&completeCondition);
+			}
+			
+			pthread_mutex_unlock(&mutex);
+		}
+	}
 	
-		thread.start(StartFeatureThread, (void*)this);
+	void Feature::stop() {
+		// Signal the thread to stop waiting for a new calculation so that it can stop.
+		pthread_mutex_lock(&mutex);
+		running = false;
+		pthread_cond_signal(&recalculateCondition);
 		
-		history->addValue(value);
+		pthread_mutex_unlock(&mutex);
 	}
 	
-	void Feature::waitForCompletion() {
-		thread.wait();
+	/*--------------*
+	 * Calculation. *
+	 *--------------*/
+	
+	// Routine that users call to request a calculation.
+	void Feature::calculate(CircularArray* input_in) {
+		// Set the input buffer, so we have access to it.
+		pthread_mutex_lock(&mutex);
+		input = input_in;
+		
+		recalculate = true;
+		
+		pthread_cond_signal(&recalculateCondition);
+		
+		pthread_mutex_unlock(&mutex);
 	}
 	
-	double Feature::getValue() {
-		return value;
-	}
-	
-	CircularArray* Feature::getHistory() {
-		return history;
-	}
-	
-	double Feature::getHistoryFrame(int i) {
-		return history->getValueAt(i);
-	}
-	
+	// Wraps performCalculation()--initializes the feature, if necessary, and normalizes the value.
 	void Feature::prepareCalculation() {
 		if (!initialized) {
 			freeMemory();
@@ -51,18 +91,65 @@ namespace Sirens {
 		value = lerp(value, min, max);
 	}
 	
+	// Routine that users call to wait for the calculation to complete.
+	void Feature::waitForCompletion() {
+		pthread_mutex_lock(&mutex);
+		
+		if (!complete)
+			pthread_cond_wait(&completeCondition, &mutex);
+					
+		complete = false;
+		pthread_mutex_unlock(&mutex);
+	}
+	
+	// Return the feature value.
+	double Feature::getValue() {
+		double return_value = -1;
+		
+		pthread_mutex_lock(&mutex);
+		return_value = value;
+		pthread_mutex_unlock(&mutex);
+		
+		return return_value;
+	}
+	
+	/*----------*
+	 * History. *
+	 *----------*/
+	
+	// These don't need to be locked, because CircularArray is thread-safe, and the history pointer never changes.
+	CircularArray* Feature::getHistory() {
+		return history;
+	}
+	
+	double Feature::getHistoryFrame(int i) {
+		return history->getValue(i);
+	}
+	
+	int Feature::getHistorySize() {
+		return history->getSize();
+	}
+	
+	int Feature::getMaxHistorySize() {
+		return history->getMaxSize();
+	}
+	
 	void Feature::setMaxHistorySize(int history_size) {
-		CircularArray* new_history = new CircularArray(history_size, false);
+		CircularArray* new_history = new CircularArray(history_size);
 		
 		if (history) {
 			for (int i = 0; i < history->getSize(); i++)
-				history->addValue(history->getValueAt(i));
+				history->addValue(history->getValue(i));
 				
 			delete history;
 		}
 		
 		history = new_history;
 	}
+	
+	/*----------------*
+	 * Normalization. *
+	 *----------------*/
 	
 	void Feature::setMax(double value) {
 		max = value;
@@ -80,17 +167,17 @@ namespace Sirens {
 		return min;
 	}
 	
+	/*---------------*
+	 * Segmentation. *
+	 *---------------*/
+	
 	SegmentationParameters* Feature::getSegmentationParameters() {
 		return segmentationParameters;
 	}
 	
-	int Feature::getMaxHistorySize() {
-		return history->getMaxSize();
-	}
-	
-	int Feature::getHistorySize() {
-		return history->getSize();
-	}
+	/*------------------------*
+	 * String representation. *
+	 *------------------------*/
 	
 	string Feature::toString() {
 		return "Feature";
--- source/Feature.h
+++ source/Feature.h
@@ -1,68 +1,77 @@
 #ifndef FEATURE_H
 #define FEATURE_H
 
+#include <pthread.h>
 #include <string>
 using namespace std;
 
-#include "stk/Thread.h"
-using namespace stk;
-
+#include "Thread.h"
 #include "CircularArray.h"
 #include "SegmentationParameters.h"
 
-// No need for a mutex, since we are guaranteed to only access data when thread is complete.
 namespace Sirens {
-	class Feature : Thread {
+	class Feature : public Thread {
 	private:
-		Thread thread;
+		// Concurrency.
+		bool running;								// whether or not the thread is running. for exiting the run loop.
+		bool recalculate;
+		bool complete;
+		
+		pthread_mutex_t mutex;
+		pthread_cond_t recalculateCondition;
+		pthread_cond_t completeCondition;					
+		
+		// Segmentation.
+		SegmentationParameters* segmentationParameters;
+		
+		// History of every feature value calculated.
+		CircularArray* history;
+		
+		// Normalization. Values are scaled between min and max.
+		double min, max;
 		
 	protected:
-		bool initialized;
-		
-		CircularArray* input, *history;
-		double value, min, max;
+		CircularArray* input;						// input buffer to perform calculations on.
+		bool initialized;							// whether or not the feature has been initialized.
+		double value;								// actual value calculated.
 			
-		virtual void initialize() {};
-		virtual void freeMemory() {};
-		
-		SegmentationParameters* segmentationParameters;
+		virtual void initialize() {};				// on-demand initialization, as some parameters may not be set on construction.
+		virtual void freeMemory() {};				// called on re-initialization, to free up any memory allocated at initialization.
 		
 	public:
 		Feature(int history_size = 1);
 		virtual ~Feature();
 		
-		void calculate(CircularArray* input_in);
-		void waitForCompletion();
+		void run();									// waits for signal to calculate, calculates, loops.
+		void stop();								// turns off the run loop. Does not destroy the thread.
 		
+		// Calculation.
+		void calculate(CircularArray* input_in);
+		void prepareCalculation();
+		virtual void performCalculation() {}		// actual implementation.
+		void waitForCompletion();					// blocks until the calculation is complete.
 		double getValue();
+		
+		// Access to feature value history.
 		CircularArray* getHistory();
 		double getHistoryFrame(int i);
-		
-		virtual string toString();
-		virtual void performCalculation() {}
-		
-		void prepareCalculation();
-		
-		void setMaxHistorySize(int history_size);
-		int getMaxHistorySize();
 		int getHistorySize();
+		int getMaxHistorySize();
+		void setMaxHistorySize(int history_size);
 		
+		// Normalization.
 		void setMax(double value);
 		void setMin(double value);
 		double getMax();
 		double getMin();
 		
+		// Segmentation.
 		SegmentationParameters* getSegmentationParameters();
 		
-		string historyString();
+		// String representation.
+		virtual string toString();
+		string historyString();	
 	};
-	
-	static inline THREAD_RETURN THREAD_TYPE StartFeatureThread(void* user) {
-		Feature* feature = (Feature*)user;
-		feature->prepareCalculation();
-		
-		return 0;
-	}
 }
 
 #endif
