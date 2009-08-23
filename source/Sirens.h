f9588dbf3c56c3c91bb8602b092f2a150b582674
Brandon Mechtley <bmechtley+github@gmail.com>
2009-08-23 14:17:24 -0700

    removed some files

--- source/Sirens.h
+++ source/Sirens.h
@@ -1,12 +1,11 @@
 #ifndef __SIRENS_H__
 #define __SIRENS_H__
 
-#include "CircularArray.h"
 #include "Feature.h"
 #include "FeatureSet.h"
 #include "SegmentationParameters.h"
 #include "Segmenter.h"
-#include "Sound.h"
+#include "Thread.h"
 #include "features/Harmonicity.h"
 #include "features/Loudness.h"
 #include "features/SpectralCentroid.h"
