012c3512b544355c9e07cde6ab1d8a91ed9d9a79
Brandon Mechtley <bmechtley+github@gmail.com>
2009-08-24 14:38:52 -0700

    parameter value changes

--- examples/segmentation.cpp
+++ examples/segmentation.cpp
@@ -32,37 +32,37 @@ int main() {
 	loudness->getSegmentationParameters()->setPLagPlus(0.75);
 	loudness->getSegmentationParameters()->setPLagMinus(0.75);
 	
-	TemporalSparsity* temporal_sparsity = new TemporalSparsity(sound->getFrameCount());
-	temporal_sparsity->setMin(1.0 / 50.0);
-	temporal_sparsity->setMax(0.5247);
-	temporal_sparsity->getSegmentationParameters()->setAlpha(0.05);
-	temporal_sparsity->getSegmentationParameters()->setR(0.0196);
-	temporal_sparsity->getSegmentationParameters()->setCStayOff(0.001833506);
-	temporal_sparsity->getSegmentationParameters()->setCTurnOn(0.85);
-	temporal_sparsity->getSegmentationParameters()->setCTurningOn(0.85);
-	temporal_sparsity->getSegmentationParameters()->setCTurnOff(0.85);
-	temporal_sparsity->getSegmentationParameters()->setCNewSegment(0.85);
-	temporal_sparsity->getSegmentationParameters()->setCStayOn(0.009296018);
-	temporal_sparsity->getSegmentationParameters()->setPLagPlus(0.75);
-	temporal_sparsity->getSegmentationParameters()->setPLagMinus(0.075);
+	SpectralCentroid* spectral_centroid = new SpectralCentroid(sound->getFrameCount(), sound->getSpectrumSize(), sound->getSampleRate());
+	spectral_centroid->setMin(0.4994);
+	spectral_centroid->setMax(25.7848);
+	spectral_centroid->getSegmentationParameters()->setAlpha(0.05);
+	spectral_centroid->getSegmentationParameters()->setR(0.00000196);
+	spectral_centroid->getSegmentationParameters()->setCStayOff(0.0000933506);
+	spectral_centroid->getSegmentationParameters()->setCTurnOn(0.85);
+	spectral_centroid->getSegmentationParameters()->setCTurningOn(0.85);
+	spectral_centroid->getSegmentationParameters()->setCTurnOff(0.85);
+	spectral_centroid->getSegmentationParameters()->setCNewSegment(0.85);
+	spectral_centroid->getSegmentationParameters()->setCStayOn(0.0025296018);
+	spectral_centroid->getSegmentationParameters()->setPLagPlus(0.75);
+	spectral_centroid->getSegmentationParameters()->setPLagMinus(0.075);
 	
 	SpectralSparsity* spectral_sparsity = new SpectralSparsity(sound->getFrameCount());
-	spectral_sparsity->setMin(1.0 / 1024.0);
+	spectral_sparsity->setMin(0);
 	spectral_sparsity->setMax(0.6509);
 	spectral_sparsity->getSegmentationParameters()->setAlpha(0.05);
 	spectral_sparsity->getSegmentationParameters()->setR(0.0196);
-	spectral_sparsity->getSegmentationParameters()->setCStayOff(0.00000833506);
+	spectral_sparsity->getSegmentationParameters()->setCStayOff(0.001833506);
 	spectral_sparsity->getSegmentationParameters()->setCTurnOn(0.85);
 	spectral_sparsity->getSegmentationParameters()->setCTurningOn(0.85);
 	spectral_sparsity->getSegmentationParameters()->setCTurnOff(0.85);
 	spectral_sparsity->getSegmentationParameters()->setCNewSegment(0.85);
-	spectral_sparsity->getSegmentationParameters()->setCStayOn(0.0019296018);
+	spectral_sparsity->getSegmentationParameters()->setCStayOn(.009296018);
 	spectral_sparsity->getSegmentationParameters()->setPLagPlus(0.75);
 	spectral_sparsity->getSegmentationParameters()->setPLagMinus(0.75);
 	
 	FeatureSet* features = new FeatureSet();
 	features->addSampleFeature(loudness);
-	features->addSampleFeature(temporal_sparsity);
+	features->addSpectralFeature(spectral_centroid);
 	features->addSpectralFeature(spectral_sparsity);
 	
 	// Extract features.	
@@ -83,7 +83,7 @@ int main() {
 	delete segmenter;
 	delete features;
 	delete loudness;
-	delete temporal_sparsity;
+	delete spectral_centroid;
 	delete spectral_sparsity;
 	
 	return 0;
