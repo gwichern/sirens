1fbf9e42a731b2de5ad2084a8166485b21521226
Brandon Mechtley <bmechtley+github@gmail.com>
2009-08-25 20:06:31 -0700

    licensing

--- source/support/string_support.cpp
+++ source/support/string_support.cpp
@@ -1,11 +1,21 @@
 /*
- *  string_support.cpp
- *  SIRENS
- *
- *  Created by Brandon Mechtley on 8/7/07.
- *  Copyright 2007 Brandon Mechtley. All rights reserved.
- *
- */
+	Copyright 2007, 2008, 2009 Brandon Mechtley
+
+	This file is part of Sirens.
+
+	Sirens is free software: you can redistribute it and/or modify
+	it under the terms of the GNU General Public License as published by
+	the Free Software Foundation, either version 3 of the License, or
+	(at your option) any later version.
+
+	Sirens is distributed in the hope that it will be useful,
+	but WITHOUT ANY WARRANTY; without even the implied warranty of
+	MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+	GNU General Public License for more details.
+
+	You should have received a copy of the GNU General Public License
+	along with Sirens.  If not, see <http://www.gnu.org/licenses/>.
+*/
 
 #include <sstream>
 #include <fstream>
@@ -16,15 +26,14 @@ using namespace std;
 
 namespace Sirens {
 	void tokenise(const string& str, vector<string>& tokens, const string& delimiters = " ") {
-	    string::size_type lastPos = str.find_first_not_of(delimiters, 0);		// Skip delimiters at beginning.
-	    string::size_type pos = str.find_first_of(delimiters, lastPos);			// Find first non-delimiter.
-
-
-	    while (string::npos != pos || string::npos != lastPos) {
-	        tokens.push_back(str.substr(lastPos, pos - lastPos));		        // Found a token, add it to the vector.
-	        lastPos = str.find_first_not_of(delimiters, pos);					// Skip delimiters.  Note the not_of.
-	        pos = str.find_first_of(delimiters, lastPos);				        // Find next non-delimiter.
-	    }
+		string::size_type lastPos = str.find_first_not_of(delimiters, 0);		// Skip delimiters at beginning.
+		string::size_type pos = str.find_first_of(delimiters, lastPos);			// Find first non-delimiter.
+		
+		while (string::npos != pos || string::npos != lastPos) {
+			tokens.push_back(str.substr(lastPos, pos - lastPos));		        // Found a token, add it to the vector.
+			lastPos = str.find_first_not_of(delimiters, pos);					// Skip delimiters.  Note the not_of.
+			pos = str.find_first_of(delimiters, lastPos);				        // Find next non-delimiter.
+		}
 	}
 
 	string double_to_string(double data) {
