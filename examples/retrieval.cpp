267cca0e8441fc6de29809ad82636b2da7e9c093
Brandon Mechtley <bmechtley+github@gmail.com>
2009-08-31 12:22:13 -0700

    example for comparing a bunch of sounds

--- examples/retrieval.cpp
+++ examples/retrieval.cpp
@@ -19,47 +19,123 @@
 using namespace std;
 
 #include "../source/Sirens.h"
+#include "../source/support/matrix_support.h"
 #include "../source/support/string_support.h"
 using namespace Sirens;
 
+#include <boost/numeric/ublas/io.hpp>
+
 int main() {
 	// Open sound.
-	Sound* sound = new Sound();
-	sound->setFrameLength(0.04);
-	sound->setHopLength(0.02);
-	sound->open("sound.wav");
+	vector<string> files(4);
+	files[0] = "sounds/whistle1.wav";
+	files[1] = "sounds/whistle2.wav";
+	files[2] = "sounds/whistle3.wav";
+	files[3] = "sounds/whistle4.wav";
 	
-	// Initialize features.	
-	Loudness* loudness = new Loudness(sound->getFrameCount() - 1);
-	loudness->getSegmentationParameters()->setAlpha(0.15);
-	loudness->getSegmentationParameters()->setR(0.0098);
-	loudness->getSegmentationParameters()->setCStayOff(0.0015);
-	loudness->getSegmentationParameters()->setCTurnOn(0.085);
-	loudness->getSegmentationParameters()->setCTurningOn(0.085);
-	loudness->getSegmentationParameters()->setCTurnOff(0.085);
-	loudness->getSegmentationParameters()->setCNewSegment(0.085);
-	loudness->getSegmentationParameters()->setCStayOn(0.05);
-	loudness->getSegmentationParameters()->setPLagPlus(0.75);
-	loudness->getSegmentationParameters()->setPLagMinus(0.75);
+	// We have three of everything, because we have two files and (unfortunately) features can't be shared.
+	vector<Sound*> sounds(files.size());
+	vector<Loudness*> loudness(files.size());
+	vector<TemporalSparsity*> temporal_sparsity(files.size());
+	vector<SpectralSparsity*> spectral_sparsity(files.size());
+	vector<SpectralCentroid*> spectral_centroid(files.size());
+	vector<TransientIndex*> transient_index(files.size());
+	vector<Harmonicity*> harmonicity(files.size());
+	vector<FeatureSet*> feature_sets(files.size());
+	vector<SoundComparator*> comparators(files.size());
 	
-	FeatureSet* features = new FeatureSet();
-	features->addSampleFeature(loudness);
+	cout << endl;
+	cout << "******************" << endl;
+	cout << "* INITIALIZATION *" << endl;
+	cout << "******************" << endl; 
+	cout << endl;
+	
+	// Initialize sounds.
+	for (int i = 0; i < files.size(); i++) {
+		// Initialize the sound files.
+		sounds[i] = new Sound();
+		sounds[i]->setFrameLength(0.04);
+		sounds[i]->setHopLength(0.02);
+		sounds[i]->open(files[i]);
 		
-	// Extract features.	
-	sound->setFeatures(features);
-	sound->extractFeatures();	
-	sound->close();
+		// The first frame of TransientIndex is junk, so record everything except the first frame.
+		int frames = sounds[i]->getFrameCount() - 1;
+		
+		int spectrum_size = sounds[i]->getSpectrumSize();
+		int sample_rate = sounds[i]->getSampleRate();
+		
+		// Initialize the features.
+		loudness[i] = new Loudness(frames);
+		transient_index[i] = new TransientIndex(frames, spectrum_size, sample_rate, 30, 15);
+		temporal_sparsity[i] = new TemporalSparsity(frames, 50);
+		spectral_sparsity[i] = new SpectralSparsity(frames);
+		spectral_centroid[i] = new SpectralCentroid(frames, spectrum_size, sample_rate);
+		harmonicity[i] = new Harmonicity(frames, spectrum_size, sample_rate);
+		harmonicity[i]->setAbsThreshold(1);
+		harmonicity[i]->setThreshold(0.1);
+		harmonicity[i]->setSearchRegionLength(5);
+		harmonicity[i]->setMaxPeaks(3);
+		harmonicity[i]->setLPFCoefficient(0.7);
+		
+		// Initialize the feature set.
+		feature_sets[i] = new FeatureSet();
+		feature_sets[i]->addSampleFeature(loudness[i]);
+		feature_sets[i]->addSpectralFeature(spectral_centroid[i]);
+		feature_sets[i]->addSpectralFeature(spectral_sparsity[i]);
+		feature_sets[i]->addSampleFeature(temporal_sparsity[i]);
+		feature_sets[i]->addSpectralFeature(transient_index[i]);
+		feature_sets[i]->addSpectralFeature(harmonicity[i]);
+		
+		// Extract features.
+		sounds[i]->setFeatures(feature_sets[i]);
+		sounds[i]->extractFeatures();
+		sounds[i]->close();
+		
+		// Initialize the comparators.
+		cout << files[i] << endl;
+		
+		comparators[i] = new SoundComparator(feature_sets[i]);
+		comparators[i]->initialize();
+	}
 	
-	// Segment sound.
-	RetrievalModel* loudness_comparator = new RetrievalModel(loudness);
-	loudness_comparator->initialize();
-	write_csv_file("smoothed_loudness.csv", *(loudness_comparator->getTrajectory()));
+	ublas::matrix<double> likelihood(files.size(), files.size());
+	
+	cout << endl;
+	cout << "**************" << endl;
+	cout << "* COMPARISON *" << endl;
+	cout << "**************" << endl; 
+	cout << endl;
+
+	// Compare each sound to itself and the other sound.
+	for (int i = 0; i < files.size(); i++) {
+		for (int j = 0; j < files.size(); j++) {
+			cout << "(" << i << ", " << j << ")" << endl;
+			likelihood(i, j) = comparators[i]->compare(comparators[j]);
+		}
+	}
+	
+	ublas::matrix<double> affinity = normalize_affinity(likelihood);
+	
+	cout << endl;
+	cout << "***********" << endl;
+	cout << "* RESULTS *" << endl;
+	cout << "***********" << endl; 
+	cout << endl;
+	
+	cout << "Likelihood: " << likelihood << endl;
+	cout << "Affinity: " << affinity << endl << endl;
 	
 	// Clean up.
-	delete sound;
-	delete features;
-	delete loudness_comparator;
-	delete loudness;
+	for (int i = 0; i < files.size(); i++) {
+		delete sounds[i];
+		delete comparators[i];
+		delete feature_sets[i];
+		delete loudness[i];
+		delete spectral_sparsity[i];
+		delete spectral_centroid[i];
+		delete transient_index[i];
+		delete harmonicity[i];
+	}
 	
 	return 0;
 }
