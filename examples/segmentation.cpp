fdedaa002f83beb72e759c248ad3d5b4643acdcb
Brandon Mechtley <bmechtley+github@gmail.com>
2009-09-03 10:16:12 -0700

    accidentally commit change to example sound path

--- examples/segmentation.cpp
+++ examples/segmentation.cpp
@@ -27,7 +27,7 @@ int main() {
 	Sound* sound = new Sound();
 	sound->setFrameLength(0.04);
 	sound->setHopLength(0.02);
-	sound->open("/Users/brandon/Documents/git/sirens/examples/sounds/whistle4.wav");
+	sound->open("sounds/whistle4.wav");
 	
 	cout << "sounds/whistle4.wav" << endl;
 	cout << "\tSamples: " << sound->getSampleCount() << endl;
